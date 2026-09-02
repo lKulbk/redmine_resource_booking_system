24a2117fc452c25b6d56394da6af900a601feec9
Liudmila Kulbikayan <113862595+lKulbk@users.noreply.github.com>
2026-09-02 14:31:40 +0300

    Update rrbs_issue_form_hook.rb

--- lib/rrbs_issue_form_hook.rb
+++ lib/rrbs_issue_form_hook.rb
@@ -1,4 +1,4 @@
 class RrbsIssueFormHook < Redmine::Hook::ViewListener
   render_on :view_issues_form_details_bottom,
-            partial: 'rrbs_hooks/issue_form_departments'
+            partial: 'rrbs_settings/issue_form_departments'
 end
